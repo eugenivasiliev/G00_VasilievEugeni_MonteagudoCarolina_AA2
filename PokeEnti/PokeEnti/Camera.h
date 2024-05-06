5419d339d35a4804890ab5a3fb0385665e7ce624
eugenivasiliev <159423029+eugenivasiliev@users.noreply.github.com>
2024-05-06 23:53:11 +0200

    Deleted camera destructor declaration

--- PokeEnti/PokeEnti/Camera.h
+++ PokeEnti/PokeEnti/Camera.h
@@ -29,7 +29,6 @@ public:
 	Camera(const std::pair<int, int>& position, const int& width, const int& height, Map* map);
 	void draw();
 	void move(const short &direction, const std::pair<int, int>& playerPosition);
-	~Camera();
 private:
 	std::ostringstream buffer;
 	std::pair<int, int> m_position;
