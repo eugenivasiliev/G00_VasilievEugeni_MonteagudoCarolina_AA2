d33d7424c6c0dd87ace3675ee4252f95551e0c45
Eugeni Vasiliev Vovk <eugeni.vasiliev@enti.cat>
2024-05-24 13:18:35 +0200

    Corrected project according to teacher's indications

--- PokeEnti/PokeEnti/InputManager.cpp
+++ PokeEnti/PokeEnti/InputManager.cpp
@@ -1,7 +1,12 @@
 #include "InputManager.h"
 #include <Windows.h>
 
-InputManager::InputManager() {}
+#define KEYCODE_D 0x44
+#define KEYCODE_F 0x46
+#define KEYCODE_J 0x4A
+#define KEYCODE_K 0x4B
+
+InputManager::InputManager() { m_inputs = {}; }
 
 short InputManager::GetCurrentInput() {
 	if (m_inputs.empty()) return NULL;
@@ -15,10 +20,10 @@ void InputManager::ProcessInput() {
 	if (m_inputs.size() >= MAX_INPUTS_BUFFERED) return;
 
 	//Movement inputs
-	if (GetAsyncKeyState(VK_LEFT) && (m_inputs.empty() || m_inputs.back() != VK_LEFT)) m_inputs.push(VK_LEFT);
-	if (GetAsyncKeyState(VK_UP) && (m_inputs.empty() || m_inputs.back() != VK_UP)) m_inputs.push(VK_UP);
-	if (GetAsyncKeyState(VK_RIGHT) && (m_inputs.empty() || m_inputs.back() != VK_RIGHT)) m_inputs.push(VK_RIGHT);
-	if (GetAsyncKeyState(VK_DOWN) && (m_inputs.empty() || m_inputs.back() != VK_DOWN)) m_inputs.push(VK_DOWN);
+	if (GetAsyncKeyState(KEYCODE_D) && (m_inputs.empty() || m_inputs.back() != KEYCODE_D)) m_inputs.push(KEYCODE_D);
+	if (GetAsyncKeyState(KEYCODE_F) && (m_inputs.empty() || m_inputs.back() != KEYCODE_F)) m_inputs.push(KEYCODE_F);
+	if (GetAsyncKeyState(KEYCODE_J) && (m_inputs.empty() || m_inputs.back() != KEYCODE_J)) m_inputs.push(KEYCODE_J);
+	if (GetAsyncKeyState(KEYCODE_K) && (m_inputs.empty() || m_inputs.back() != KEYCODE_K)) m_inputs.push(KEYCODE_K);
 
 	//Interaction
 	if (GetAsyncKeyState(VK_SPACE) && (m_inputs.empty() || m_inputs.back() != VK_SPACE)) m_inputs.push(VK_SPACE);
