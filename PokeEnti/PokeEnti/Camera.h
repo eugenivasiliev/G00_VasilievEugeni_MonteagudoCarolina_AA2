49036ed58fdff655cd818c40a9d78c08833aa973
eugenivasiliev <159423029+eugenivasiliev@users.noreply.github.com>
2024-06-02 15:56:02 +0200

    Update Camera.h

--- PokeEnti/PokeEnti/Camera.h
+++ PokeEnti/PokeEnti/Camera.h
@@ -1,27 +1,10 @@
 #pragma once
 
 #include "Map.h"
+#include "Util.h"
 #include <utility>
 #include <Windows.h>
 #include <sstream>
-
-extern const std::pair<int, int> UP;
-extern const std::pair<int, int> DOWN;
-extern const std::pair<int, int> LEFT;
-extern const std::pair<int, int> RIGHT;
-
-extern const char EMPTY_TILE;
-extern const char WALL_TILE;
-extern const char POKEMON_TILE;
-
-extern const char UP_SPRITE;
-extern const char DOWN_SPRITE;
-extern const char LEFT_SPRITE;
-extern const char RIGHT_SPRITE;
-
-std::pair<int, int> operator+ (const std::pair<int, int>& l, const std::pair<int, int>& r);
-void operator+= (std::pair<int, int>& l, const std::pair<int, int>& r);
-bool operator< (const std::pair<int, int> &l, const std::pair<int, int> &r);
 
 class Camera {
 public:
