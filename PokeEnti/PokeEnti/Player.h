41e21569b77ac7187aaade86d7c7f8d316491ac9
eugenivasiliev <159423029+eugenivasiliev@users.noreply.github.com>
2024-06-02 15:59:28 +0200

    Update Player.h

--- PokeEnti/PokeEnti/Player.h
+++ PokeEnti/PokeEnti/Player.h
@@ -1,24 +1,10 @@
 #pragma once
 #include "Map.h"
+#include "Util.h"
 #include <Windows.h>
 #include <utility>
 
 #define STARTING_POKEMON 0
-
-extern const std::pair<int, int> UP;
-extern const std::pair<int, int> DOWN;
-extern const std::pair<int, int> LEFT;
-extern const std::pair<int, int> RIGHT;
-
-extern const char EMPTY_TILE;
-
-extern const char UP_SPRITE;
-extern const char DOWN_SPRITE;
-extern const char LEFT_SPRITE;
-extern const char RIGHT_SPRITE;
-
-std::pair<int, int> operator+ (const std::pair<int, int>& l, const std::pair<int, int>& r);
-void operator+= (std::pair<int, int>& l, const std::pair<int, int>& r);
 
 class Player
 {
@@ -26,14 +12,16 @@ public:
 	Player();
 	Player(const int &x, const int &y);
 	std::pair<int, int> getPosition() const;
-	char getSprite() const;
+	PlayerTiles getSprite() const;
 	int getCapturedPokemon() const;
 	void move(const short &direction, const Map &tiles);
 	void incrementPokemon(const bool &captured);
 	~Player();
 private:
 	std::pair<int, int> m_position;
-	char m_orientation;
+	PlayerTiles m_orientation;
 	int m_capturedPokemon;
 };
+
+
 
