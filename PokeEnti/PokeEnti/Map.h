075672a72d8ada2e7986cdf682c9d30287758d75
eugenivasiliev <159423029+eugenivasiliev@users.noreply.github.com>
2024-06-02 15:58:41 +0200

    Update Map.h

--- PokeEnti/PokeEnti/Map.h
+++ PokeEnti/PokeEnti/Map.h
@@ -1,18 +1,7 @@
 #pragma once
 #include "ReadDoc.h"
+#include "Util.h"
 #include <utility>
-
-extern const std::pair<int, int> UP;
-extern const std::pair<int, int> DOWN;
-extern const std::pair<int, int> LEFT;
-extern const std::pair<int, int> RIGHT;
-
-extern const char EMPTY_TILE;
-extern const char WALL_TILE;
-extern const char POKEMON_TILE;
-
-std::pair<int, int> operator+ (const std::pair<int, int>& l, const std::pair<int, int>& r);
-bool operator< (const std::pair<int, int>& l, const std::pair<int, int>& r);
 
 class Zone {
 public:
@@ -31,17 +20,17 @@ class Map
 public:
 	Map();
 	Map(const Data &data);
-	void update(const std::pair<int, int>& playerPosition, const char& playerSprite, const int& capturedPokemon);
+	void update(const std::pair<int, int>& playerPosition, const PlayerTiles& playerSprite, const int& capturedPokemon);
 	int getWidth() const;
 	int getHeight() const;
 	bool checkPokemon(std::pair<int, int> position);
-	char operator() (const std::pair<int, int> &position) const;
-	char operator() (const int &x, const int &y) const;
+	Tiles operator() (const std::pair<int, int> &position) const;
+	Tiles operator() (const int &x, const int &y) const;
 	~Map();
 
 private:
 	int m_width, m_height;
-	char** m_tiles;
+	Tiles** m_tiles;
 
 	std::pair<int, int> getRandomEmptyTile() const;
 	void repositionPokemon(std::pair<int, int> position);
